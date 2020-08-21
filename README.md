b3d0f17d0317c5a28eadc3e6acaeef05723d2ea9
Elle <ellesummer212@gmail.com>
2020-08-21 16:39:57 -0400

    slight code refactor and removed claw image for precision reasons

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-### Setup
-% cd TwoJointBot
-% pipenv --three install
+# 2-Joint Arm Simulator
+Uses inverse kinematics to bend arm's joints appropriately to reach cursor. Hardware design for arm is also included to be built whenever the pandemic is over. In the meantime, I built a small web app to simulate the arm.
+![Alt_text](twojointbot/frontend/static/images/twojointarm_app_screenshot.png)
+#### Setup
+% cd TwoJointBot  
+% pipenv --three install  
 % pipenv run python script.py
+
+#### Acknowledgements
+Mechanical design: Aidan Miller  
+Render and design schematic: Murun Tsogtkhuyag  
--- twojointbot/frontend/static/arm_viewer.js
+++ twojointbot/frontend/static/arm_viewer.js
@@ -5,22 +5,16 @@ const height = canvas.height;
 ctx.lineWidth = 2;
 const tableHeight = 5*height/6;
 const scale = 1.0;
-const home_y = tableHeight - 2;
-const home_x = width / 2;
-const hand_home_x = 143;
-const hand_home_y = 85;
+const base_home_y = tableHeight-10;
+const base_home_x = width / 2 + 50;
+const hand_home_x = width/4;
+const hand_home_y = height/6 - 21;
 const canvasOffset=$("#canvas").offset();
 const offsetX=canvasOffset.left;
 const offsetY=canvasOffset.top;
-let init_claw_ang = 45;
-let init_bic_ang = 45;
-let prev_claw_ang;
-let claw = document.getElementById("claw");
-const claw_offsetX = -15;// claw.width;
-const claw_offsetY = -15;// claw.height;
 
-function set_up() {
-    moveImage("base", home_x-200, home_y);
+function draw_screen() {
+    move_image("base", base_home_x-200, base_home_y);
     draw_initial_arm();
     set_color('#000000');
     draw_axes();
@@ -28,7 +22,7 @@ function set_up() {
 
 function reset() {
     clear_screen();
-    set_up();
+    draw_screen();
 }
 
 function draw_table() {
@@ -48,28 +42,15 @@ function draw_arm(arm_data) {
     clear_screen();
     ctx.lineWidth = 10;
     // draw first arm segment
-    // console.log("actual" + arm_data['elbow_y'])
-    // console.log("scale" + scale)
-    // console.log("scaled" + scale*arm_data['elbow_y'])
-    let to_x1 = home_x + scale*arm_data['elbow_x'];
-    let to_y1 = home_y - scale*arm_data['elbow_y'];
+    let to_x1 = base_home_x + scale*arm_data['elbow_x'];
+    let to_y1 = base_home_y - scale*arm_data['elbow_y'];
     let YELLOW = "#FFCC00";
-    draw_line([home_x, home_y], [to_x1, to_y1], YELLOW)
+    draw_line([base_home_x, base_home_y+10], [to_x1, to_y1], YELLOW)
     // draw second arm segment
-    let to_x2 = home_x + scale*arm_data['hand_x'];
-    let to_y2 = home_y - scale*arm_data['hand_y'];
+    let to_x2 = base_home_x + scale*arm_data['hand_x'];
+    let to_y2 = base_home_y - scale*arm_data['hand_y'];
     let RED = "#FF0000";
     draw_line([to_x1, to_y1], [to_x2, to_y2], RED)
-}
-
-function update_claw_angle(arm_data) {
-    moveImage("claw", hand_home_x+home_x,
-        hand_home_y+290);
-    let bicep_angle = parseFloat(arm_data['bicep_angle']);
-    let forearm_angle = parseFloat(arm_data['forearm_angle']);
-    let claw_angle = -forearm_angle-bicep_angle;
-    rotateImage("claw", -(init_claw_ang-claw_angle), "top left");
-    prev_claw_ang = claw_angle;
 }
 
 function draw_initial_arm() {
@@ -77,7 +58,6 @@ function draw_initial_arm() {
     fetch('/get_arm_data').then(function(data){
         return data.json();
     }).then(function (arm_data) {
-        update_claw_angle(arm_data);
         draw_arm(arm_data);
     }).catch((error) => {
       console.error('Error:', error);
@@ -116,13 +96,11 @@ function set_hand_pos(x, y) {
     });
 }
 
-$("body").mousemove(function(e){handleMouseMove(e);});
-function handleMouseMove(e) {
+$("body").mousemove(function(e){handle_mouse_move(e);});
+function handle_mouse_move(e) {
     let mouseX=parseInt(e.clientX-offsetX);
     let mouseY=parseInt(e.clientY-offsetY);
-    // restrict claw height to table
-    if (mouseY >= home_y - 60) return;
-    const message = {'x': mouseX - home_x, 'y': home_y - mouseY};
+    const message = {'x': mouseX - base_home_x, 'y': base_home_y - mouseY};
     fetch('/get_arm_data', {
         method: 'POST',
         body: JSON.stringify(message)
@@ -133,17 +111,6 @@ function handleMouseMove(e) {
             // console.log("Can't reach.");
         }
         else { //success
-            update_claw_angle(arm_data);
-            // code to try to fix the claw offset issue
-            // let adjust = tanFromDegrees(prev_claw_ang);
-            // console.log("adj", adjust);
-            // console.log("claw ang", prev_claw_ang);
-            // let bicep_angle = parseFloat(arm_data['bicep_angle']);
-            // let forearm_angle = parseFloat(arm_data['forearm_angle']);
-            // console.log("sho ang", forearm_angle);
-            // console.log("bic ang", bicep_angle);
-            //moveImage("claw", mouseX-claw_offsetX - 100, mouseY-claw_offsetY-adjust*100);
-            moveImage("claw", mouseX-claw_offsetX, mouseY-claw_offsetY);
             draw_arm(arm_data);
         }
     }).catch((error) => {
@@ -158,25 +125,15 @@ function clear_screen() {
     draw_axes();
 }
 
-function moveImage(IDname, newX, newY) {
-    let image = $("#" + IDname);
+function move_image(html_id, newX, newY) {
+    let image = $("#" + html_id);
     image.css("left", newX + "px");
     image.css("top", newY + "px");
 }
 
-function imgError(image) {
+function img_error(image) {
     image.onerror = "";
     image.src = "/images/noimage.gif";
     console.log("image error");
     return true;
-}
-
-function rotateImage(IDname, degrees, mode="center") {
-    let image = $("#" + IDname);
-    image.css("transform-origin", mode);
-    image.css("transform", 'rotate(' + degrees + 'deg)');
-}
-
-function tanFromDegrees(degrees) {
-  return Math.tan(degrees * Math.PI/180);
 }
